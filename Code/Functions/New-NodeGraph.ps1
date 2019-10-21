e13ed6379f6011fcef0500718816a502b9712e7a
LxLeChat <yanaski@msn.com>
2019-10-21 16:44:40 +0200

    Update New-NodeGraph.ps1

--- Code/Functions/New-NodeGraph.ps1
+++ Code/Functions/New-NodeGraph.ps1
@@ -22,23 +22,17 @@ function New-NodeGraph {
     end {
         If ( ! $PSBoundParameters["GroupAffiliatedNodes"].isPresent ) {
 
-            $graph = graph -Name "lol"  {
+            graph -Name "lol"  {
                 for ( $i =0 ; $i -lt $arrayofnodes.count; $i++ ) {
                     subgraph _$i {
-                        node -name $arrayofnodes[$i].Guid -attributes @{label=$arrayofnodes[$i]."$FindBetterVariableName"}
-                        If ($arrayofnodes[$i].Children.Count -gt 0 ) {
-                            foreach ( $n in $arrayofnodes[$i].GetChildren($true) ) {
-                                node -name $n.Guid -attributes @{label=$n."$FindBetterVariableName"}
-                                edge -From $n.parent.Guid -to $n.Guid
-                            }
+                        node -name $arrayofnodes[$i].NodeId -attributes @{label=$arrayofnodes[$i]."$FindBetterVariableName"}
+                        foreach ( $n in $arrayofnodes[$i].GetChildren($true) ) {
+                            node -name $n.NodeId -attributes @{label=$n."$FindBetterVariableName"}
+                            edge -From $n.parent.NodeId -to $n.NodeId
                         }
-
                     }
-                    If ($arrayofnodes[$i].Children.Count -gt 0 ) {
-                        edge -from $arrayofnodes[$i].getchildren($true)[$arrayofnodes[$i].GetChildren($true).Count -1].Guid -to $arrayofnodes[$i+1].Guid -attributes @{ltail="cluster_$i";lhead="cluster_$($i+1)"}
-                    } Else {
-                        edge -from $arrayofnodes[$i].Guid -to $arrayofnodes[$i+1].Guid -attributes @{ltail="cluster_$i";lhead="cluster_$($i+1)"}
-                    }
+                    edge -from $arrayofnodes[$i].getchildren($true)[$arrayofnodes[$i].GetChildren($true).Count -1].NodeId -to $arrayofnodes[$i+1].NodeId -attributes @{ltail="cluster_$i";lhead="cluster_$($i+1)"}
+                    
                 }
             }
 
@@ -47,30 +41,29 @@ function New-NodeGraph {
             $graph = graph -Name "lol" -attributes @{rankdir='LR'} {
 
                 $arrayofnodes.foreach({
-                    node $_.Guid -attributes @{label=$_."$FindBetterVariableName"}
+                    node $_.NodeId -attributes @{label=$_."$FindBetterVariableName"}
                 })
             
                 $arrayofnodes.GetChildren($True).foreach({
-                    node $_.Guid -attributes @{label=$_."$FindBetterVariableName"}
+                    node $_.NodeId -attributes @{label=$_."$FindBetterVariableName"}
                 })
             
                 for ( $i=0;$i -lt $x.count ; $i++ ) {
-                    edge -from $arrayofnodes[$i].Guid -to $arrayofnodes[$i+1].Guid
+                    edge -from $arrayofnodes[$i].NodeId -to $arrayofnodes[$i+1].NodeId
                 }
             
                 $arrayofnodes.foreach({
                     foreach ( $n in $_.getchildren($true) ) {
                         if ( $n.parent.statement -eq $arrayofnodes[$i].Statement ) {
-                            edge -From $arrayofnodes[$i].Guid -To $n.Guid
+                            edge -From $arrayofnodes[$i].NodeId -To $n.NodeId
                         } else {
-                            edge -from $n.parent.Guid -to $n.Guid
+                            edge -from $n.parent.NodeId -to $n.NodeId
                         }
                     }
                 })
             }
 
         }
-        
-        $graph
+
     }
 }
